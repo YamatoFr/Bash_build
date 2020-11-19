faae45418e1fce2aa71be1cce83e2afbab9baa87
YamatoFr <60272310+YamatoFr@users.noreply.github.com>
2020-11-18 20:03:54 -0400

    Add files via upload

---
+++ Bash Build/main.c
@@ -0,0 +1,13 @@
+/* En accord avec le groupe, les noms de variables seront en anglais */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+int main()
+{
+    bool playing = true;
+    int day = 0;
+    int choice;
+}

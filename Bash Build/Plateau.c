a6ee9217848b376825152e6a486df2ac5fc0e5e9
YamatoFr <theofigini@gmail.com>
2020-11-20 08:11:51 -0400

    Update Plateau.c

--- Bash Build/Plateau.c
+++ Bash Build/Plateau.c
@@ -4,90 +4,55 @@
 
 void *mallocP(size_t size)
 {
-	void *p = NULL;
-	p = malloc(size);
+    void *p = NULL;
+    p = malloc(size);
 
-	if (p == NULL)
-	{
-		printf("Erreur mémoire\n");
-		exit(0);
-	}
+    if (p == NULL)
+    {
+        printf("Erreur mémoire\n");
+        exit(0);
+    }
 
-	return p;
+    return p;
 }
 
 void emptyBuffer()
 {
-	int a = 0;
+    int a = 0;
 
-	while (a != "\n" && a != EOF)
-	{
-		a = getchar();
-	}
+    while (a != "\n" && a != EOF)
+    {
+        a = getchar();
+    }
 }
 
 List *insertList()
 {
-	List *List = mallocP(sizeof(List));
+    List *List = mallocP(sizeof(List));
 
-	List->head = NULL;
-	List->nbBuilds = 0;
+    List->head = NULL;
+    List->nbBuilds = 0;
 
-	return List;
+    return List;
 }
 
 Board *createBoard()
 {
-	Board *b = (Board *)mallocP(sizeof(Board));
+    Board *b = (Board *)mallocP(sizeof(Board));
 
-	b->width = 10;
-	b->height = 10;
+    b->width = 10;
+    b->height = 10;
 
-	return b;
+    return b;
 }
 
 void create2dBoard(Board **b)
 {
-	int **board = (int **)mallocP((*b)->width * sizeof(int *));
+    int **board = (int **)mallocP((*b)->width * sizeof(int *));
 
-	for (int i = 0; i < (*b)->width; i++)
-	{
-		board[i] = (int *)mallocP((*b)->height * sizeof(int));
-	}
-	(*b)->map = board;
-}
-
-void initBoard(Board **b)
-{
-	for (int i = 0; i < (*b)->width; i++)
-	{
-		for (int j = 0; j < (*b)->height; j++)
-		{
-			((*b)->map)[i][j] = 0;
-		}
-	}
-}
-
-void printBoard(Board *b)
-{
-	printf("\n");
-
-	for (int i = 0; i < b->width; i++)
-	{
-		for (int j = 0; j < b->height; j++)
-		{
-			// Formatage
-			int pb = (b->map)[i][j];
-			if (pb) // <=> pb == True
-			{
-				printf("%3d", pb); // meilleur visuel , ne pas changer
-			}
-			else
-			{
-				printf(".  "); // Représente les cases vides
-			}
-		}
-		printf("\n"); // Saut de ligne
-	}
-	printf("\n") // Saut de ligne après le tableau
+    for (int i = 0; i < (*b)->width; i++)
+    {
+        board[i] = (int *)mallocP((*b)->height * sizeof(int));
+    }
+    (*b)->map = board;
 }
